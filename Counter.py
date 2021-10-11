a811f7cddf0eb8d908e92e2ff6444fb61559d136
SyedRehana <91021740+SyedRehana@users.noreply.github.com>
2021-10-11 23:22:38 +0530

    Add files via upload

---
+++ Counter.py
@@ -0,0 +1,8 @@
+#To get the word from the user
+from collections import Counter 
+string= str(input("Enter the word:"))
+print(Counter(string))
+#To print from the given input
+from collections import Counter
+string="mississippi"
+print(Counter(string))
